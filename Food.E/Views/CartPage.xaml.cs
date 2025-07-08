391248fcf70970617bc42d2a0d6ec13aee8c707d
pirates-mukunda <mukundaa077@gmail.com>
2025-07-08 18:41:40 -0400

    Add Food.E/Views/CartPage.xaml.cs 1752014501668

--- Food.E/Views/CartPage.xaml.cs
+++ Food.E/Views/CartPage.xaml.cs
@@ -1,40 +1,20 @@
-using System;
 using Food.E.Services;
+using Food.E.ViewModels;
 using Microsoft.Maui.Controls;
 
 namespace Food.E.Views
 {
     public partial class CartPage : ContentPage
     {
-        private readonly CartService _cartService;
-
         public CartPage()
         {
             InitializeComponent();
-            _cartService = new CartService();
-            LoadCartItems();
-        }
-
-        private void LoadCartItems()
-        {
-            CartItemsCollectionView.ItemsSource = _cartService.GetCartItems();
-            TotalPriceLabel.Text = $"Total: ${_cartService.GetTotalPrice():F2}";
-        }
-
-        private void OnRemoveClicked(object sender, EventArgs e)
-        {
-            var button = sender as Button;
-            var itemId = button?.CommandParameter as string;
-            if (!string.IsNullOrEmpty(itemId))
+            var dataService = new SqlDataService();
+            var viewModel = new CartViewModel(dataService)
             {
-                _cartService.RemoveFromCart(itemId);
-                LoadCartItems();
-            }
-        }
-
-        private async void OnCheckoutClicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new CheckoutPage());
+                Navigation = this.Navigation
+            };
+            BindingContext = viewModel;
         }
     }
 }
